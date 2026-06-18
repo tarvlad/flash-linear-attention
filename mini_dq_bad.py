f5e85a1d0c9df95968047ffd85144bdb844c5f32
tarvlad <tarasov.vladislav@huawei-partners.com>
2026-06-18 10:46:31 +0700

    reproducer v4: mask on b_ds, bare exp2 no tl.where wrapping

--- mini_dq_bad.py
+++ mini_dq_bad.py
@@ -56,10 +56,10 @@ def bwd_kernel_dq(
         b_k = tl.load(p_k, boundary_check=(0, 1))
         b_v = tl.load(p_v, boundary_check=(0, 1))
         b_s = tl.dot(b_q, b_k) * scale * RCP_LN2
-        b_p = tl.where((o_q[:, None] >= o_k[None, :]) & m_k[None, :],
-                       tl.math.exp2(b_s - b_ls[:, None]), 0.0)
+        b_p = tl.math.exp2(b_s - b_ls[:, None])
         b_dp = tl.dot(b_do, b_v)
         b_ds = b_p * (b_dp.to(tl.float32) - b_dt[:, None])
+        b_ds = tl.where((o_q[:, None] >= o_k[None, :]) & m_k[None, :], b_ds, 0.0)
         b_dq += tl.dot(b_ds.to(b_k.dtype), tl.trans(b_k))
 
     b_dq *= scale
