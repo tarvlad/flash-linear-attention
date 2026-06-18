5a18d2b254eb974dc25ef45af5b2265c6ee33c75
tarvlad <tarasov.vladislav@huawei-partners.com>
2026-06-18 10:49:16 +0700

    reproducer v5: b_p.to(tl.float32) instead of p_noise store in bad

--- mini_dq_bad.py
+++ mini_dq_bad.py
@@ -57,6 +57,7 @@ def bwd_kernel_dq(
         b_v = tl.load(p_v, boundary_check=(0, 1))
         b_s = tl.dot(b_q, b_k) * scale * RCP_LN2
         b_p = tl.math.exp2(b_s - b_ls[:, None])
+        b_p = b_p.to(tl.float32)
         b_dp = tl.dot(b_do, b_v)
         b_ds = b_p * (b_dp.to(tl.float32) - b_dt[:, None])
         b_ds = tl.where((o_q[:, None] >= o_k[None, :]) & m_k[None, :], b_ds, 0.0)
